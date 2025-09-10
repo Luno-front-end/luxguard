12ca399a11bf24dec9cfc5720b492c1cf144bb8a
maksym_zaika <onlyrobochaposhta@gmail.com>
2025-09-11 02:57:56 +0300

    added lang about us

--- src/pages/AboutUs/AboutUs.tsx
+++ src/pages/AboutUs/AboutUs.tsx
@@ -9,13 +9,14 @@ import { useMenuActive, useModalActive } from "../../zustand/store";
 import { useViewportSize } from "../../hooks/useViewport";
 
 export const AboutUs = () => {
+  const { t } = useTranslation();
+
   const cards = [
-    { title: "Security without improvisation", subtitle: "" },
-    { title: "Template-free solutions", subtitle: "" },
-    { title: "Full adaptation to changes", subtitle: "" },
+    { title: t("aboutUs.secFour.cardOne.title"), subtitle: "" },
+    { title: t("aboutUs.secFour.cardTwo.title"), subtitle: "" },
+    { title: t("aboutUs.secFour.cardThree.title"), subtitle: "" },
   ];
 
-  const { t } = useTranslation();
   const { setIsOpenModal } = useModalActive();
   const { setIsOpenMenu } = useMenuActive();
 
@@ -25,93 +26,72 @@ export const AboutUs = () => {
     <>
       <div className="container">
         <div className={s.wrapper}>
-          <h1 className={s.header_top}>About US</h1>
+          <h1 className={s.header_top}>{t("aboutUs.header")}</h1>
           <div className={s.wrapper_one}>
-            <HeaderSection>LUXGUARD PRESTIGE GROUP</HeaderSection>
+            <HeaderSection>{t("aboutUs.secOne.title")}</HeaderSection>
             <p className={s.text}>
-              <span className={s.subText}>
-                is not just a security company. It is a strategic partner that
-                provides personal safety, armored transportation, VIP support,
-                crisis consulting and risk management for customers working in a
-                dynamic and high-risk environment.
-              </span>
-              <span className={s.subText}>
-                We speak the language of those responsible — leaders, diplomats,
-                international delegations and private individuals who need not
-                symbolic protection, but proven solutions based on experience,
-                threat prevention and complete privacy.
-              </span>
-              <span className={s.subText}>
-                Our team — is officers from special forces, state structures and
-                diplomatic missions, used to acting under the pressure of time,
-                in the war zone and emergency situations.
-              </span>
-              <span className={s.subText}>
-                We work at the intersection of security, mobility and
-                operational analysis, providing an accurate result where there
-                is no right to error.
-              </span>
-              <span className={s.subText}>
-                LUXGUARD operates in Ukraine, the EU and border regions —
-                wherever reliability, experience and strategic thinking are
-                needed.
-              </span>
+              <span className={s.subText}>{t("aboutUs.secOne.infoOne")}</span>
+              <span className={s.subText}>{t("aboutUs.secOne.infoTwo")}</span>
+              <span className={s.subText}>{t("aboutUs.secOne.infoThree")}</span>
+              <span className={s.subText}>{t("aboutUs.secOne.infoFour")}</span>
+              <span className={s.subText}>{t("aboutUs.secOne.infoFive")}</span>
             </p>
           </div>
           <div className={s.wrapper_two}>
-            <HeaderSection>How we work</HeaderSection>
+            <HeaderSection>{t("aboutUs.secTwo.title")}</HeaderSection>
             <h3 className={s.header_wrapper_two}>
-              Clarity. Transparency. The result.
+              {t("aboutUs.secTwo.subtitle")}
             </h3>
-            <p className={s.text}>
-              Each request — is unique. But the principles remain the same:
-              strategic planning, full coordination and control at every stage.
-              LUXGUARD PRESTIGE GROUP acts as a single management center for
-              your security, mobility, and privacy.
-            </p>
+            <p className={s.text}>{t("aboutUs.secTwo.text")}</p>
           </div>
           <div className={s.wrapper_three}>
-            <HeaderSection>Our approach — 5 key stages</HeaderSection>
+            <HeaderSection>{t("aboutUs.secThree.title")}</HeaderSection>
             <ul className={s.list}>
               <li className={s.item}>
-                <h3 className={s.headerList}>Task analysis</h3>
+                <h3 className={s.headerList}>
+                  {t("aboutUs.secThree.keys.keyOne.title")}
+                </h3>
                 <p className={s.textList}>
-                  We study the request, event format, risk level, publicity,
-                  special needs of the client.
+                  {t("aboutUs.secThree.keys.keyOne.subtitle")}
                 </p>
               </li>
               <li className={s.item}>
-                <h3 className={s.headerList}>Decision formation</h3>
+                <h3 className={s.headerList}>
+                  {t("aboutUs.secThree.keys.keyTwo.title")}
+                </h3>
                 <p className={s.textList}>
-                  We select the optimal team, vehicle, means of communication,
-                  support and technical resources.
+                  {t("aboutUs.secThree.keys.keyTwo.subtitle")}
                 </p>
               </li>
               <li className={s.item}>
-                <h3 className={s.headerList}>Approval and planning</h3>
+                <h3 className={s.headerList}>
+                  {t("aboutUs.secThree.keys.keyThree.title")}
+                </h3>
                 <p className={s.textList}>
-                  We work out the route, briefing, backup scenarios. All — with
-                  customer approval.
+                  {t("aboutUs.secThree.keys.keyThree.subtitle")}
                 </p>
               </li>
               <li className={s.item}>
-                <h3 className={s.headerList}>Implementation and support</h3>
+                <h3 className={s.headerList}>
+                  {" "}
+                  {t("aboutUs.secThree.keys.keyFour.title")}
+                </h3>
                 <p className={s.textList}>
-                  We implement the solution: the LPG — team is involved, guards,
-                  drivers, coordinators, dispatch center. Control 24/7.
+                  {t("aboutUs.secThree.keys.keyFour.subtitle")}
                 </p>
               </li>
               <li className={s.item}>
-                <h3 className={s.headerList}>Reporting and analysis</h3>
+                <h3 className={s.headerList}>
+                  {t("aboutUs.secThree.keys.keyFive.title")}
+                </h3>
                 <p className={s.textList}>
-                  After completing —, a detailed report, performance evaluation
-                  and recommendations for the future.
+                  {t("aboutUs.secThree.keys.keyFive.subtitle")}
                 </p>
               </li>
             </ul>
           </div>
           <div className={s.wrapper_four}>
-            <HeaderSection>We guarantee</HeaderSection>
+            <HeaderSection>{t("aboutUs.secFour.title")}</HeaderSection>
             <div className={s.wrapper_four_card}>
               <CardsProtection cards={cards} isOneColumn={width >= 1280} />
             </div>
